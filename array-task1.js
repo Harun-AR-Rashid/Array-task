900befd14d7903e76699e0c2424064b35cf565e3
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-02 21:48:00 +0600

    ARRAY TASK TWO DONE

---
+++ array-task1.js
@@ -0,0 +1,16 @@
+// ### 1. Declare an array 
+
+let fruits = ['mango', 'apple', 'orange', 'guajava', 'lichi']
+console.log(fruits[3]);
+fruits[2]='jambura';
+console.log(fruits);
+
+// ### 2. Add or remove elements
+let touristSpot = ['coxbazar', 'chattogram', 'sunderbon']
+
+touristSpot.push('rangamati');
+console.log(touristSpot);
+touristSpot.push('khagrachori', 'bandorban');
+console.log(touristSpot);
+touristSpot.pop();
+console.log(touristSpot);
